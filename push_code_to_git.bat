0f753fed9949bd7658c0f64226b7c7d9703fed6c
unknown <Supratip Banerjee>
2020-07-13 14:23:30 +0530

    my page

---
+++ push_code_to_git.bat
@@ -0,0 +1,13 @@
+::change location of your new repo
+git init
+git add .
+git commit -m "my page"
+git config --global user.email supratip.banerjee@yahoo.in
+git config --global user.email "Supratip Banerjee"
+
+::create the below <repo_name> in git
+::name a new repo for local the below <repo_name> in git
+::git remote add <repo> https://github.com/supratipb/<repo_name>.git
+
+git remote add cert https://github.com/supratipb/supratipb.github.io.git
+git push cert master  
